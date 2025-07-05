a8e857198dee92055358686ba2fce4668974d0f2
leeyeji <yeji1135@gachon.ac.kr>
2025-07-06 00:08:58 +0900

    [Feat] CameraPicker, View
    카메라피커 및 뷰 추가

--- ScentBridge/Modules/Tab/Camera/Views/CameraView.swift
+++ ScentBridge/Modules/Tab/Camera/Views/CameraView.swift
@@ -6,11 +6,60 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct CameraView: View {
+
+    @State private var showCamera = false
+    @State private var capturedImage: UIImage?
+    @State private var selectedItems: [PhotosPickerItem] = []
+    
+    @State private var showPhotosPicker = false
+
     var body: some View {
-        Text("Camera")
+        VStack {
+            captionBoard
+            
+            bottomBtn
+        }
     }
+    
+    private var captionBoard: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(LinearGradient.ScentBridge.primary.linearGradient)
+                .frame(width: 334, height: 347)
+            
+            Text("사진을 등록하고 노드님의 향을 찾아보세요")
+
+                
+        }
+    }
+    
+    private var bottomBtn: some View {
+        HStack {
+            cameraBtn
+            imageBtn
+        }
+    }
+    
+    private var cameraBtn: some View {
+        Button("📷 카메라 열기") {
+            showCamera = true
+        }
+        .padding()
+        .sheet(isPresented: $showCamera) {
+            CameraPicker { image in
+                self.capturedImage = image
+            }
+        }
+    }
+    
+    private var imageBtn: some View {
+        PhotosPicker("📁 사진 선택하기", selection: $selectedItems, maxSelectionCount: 5, matching: .images)
+    }
+    
+    
 }
 
 #Preview {
